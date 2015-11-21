a23e523c6f74e649913e181c7a3837e4c26c705c
sunyanan891114@gmail.com <sunyanan891114@gmail.com>
2015-11-21 17:25:56 +0800

    test homework

--- src/calculate-first.js
+++ src/calculate-first.js
@@ -1,3 +1,27 @@
 function single_element(arr) {
+	var map = {};
+	var temp = [];
+  arr.filter(function(el, index) {
+  	if(index % 2 != 0)
+  		temp.push(el);
+  	return temp;
+  });
+  console.log(temp);
+  var result = [];
+  var i = 0;
   
+  var j = 0;
+  for(j = 0; j < temp.length; j++){
+  	var el = temp[j];
+  	var flag = false;
+  	for(i = 0;i < temp.length && i != j;i++){
+  		if( temp[i] == el ){ 			
+  			flag = true;
+  			break;
+  		}
+  	}
+  	if(!flag)
+  		result.push(el);
+  }
+  return result;
 }
