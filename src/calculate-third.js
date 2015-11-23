907edd883e537e324ccd7cbeb566bfb764d5240a
sunyanan891114@gmail.com <sunyanan891114@gmail.com>
2015-11-23 14:41:33 +0800

    third problem

--- src/calculate-third.js
+++ src/calculate-third.js
@@ -1,10 +1,10 @@
 function even_group_calculate_average(arr) {
     var result = [];
     var number = [];
-    for(var i = 0; i < arr.length; i++){
-        if(i % 2 != 0 && arr[i] % 2 == 0){
-            var n = arr[i].toString().length;
-            if(result[n] == undefined){
+    for (var i = 0; i < arr.length; i++) {
+        if (i % 2 != 0 && arr[i] % 2 == 0) {
+            var n = arr[i].toString().length - 1;
+            if (result[n] == undefined) {
                 result[n] = 0;
                 number[n] = 0;
             }
@@ -12,16 +12,19 @@ function even_group_calculate_average(arr) {
             number[n]++;
         }
     }
-    for(var i = 0;i < result.length; i++){
-        if(result[i] != undefined)
+    for (i = 0; i < result.length; i++) {
+        if (result[i] != undefined)
             result[i] = result[i] / number[i];
-        else{
-            result.splice(i+1,1);
+        else {
+        }
+    }
+
+    for (i = 0; i < result.length; i++) {
+        if (result[i] === undefined) {
+            result.splice(i, 1);
             i--;
         }
     }
-    console.log("hello");
     console.log(result);
     return result;
-
 }
